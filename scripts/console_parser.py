30d764d44f71ace2bfa068a5b10373a8f0f072ed
pinchukovartur <pinchukovartur@gmail.co>
2017-08-01 11:50:45 +0300

    create console key

--- scripts/console_parser.py
+++ scripts/console_parser.py
@@ -7,8 +7,8 @@ def get_console_param():
     # read console parameter
     parser = argparse.ArgumentParser()
     parser.add_argument('g', nargs='?', default='')
-    parser.add_argument('c', nargs='?', default='* * * * *')
-    parser.add_argument('s', nargs='?', default='')
+    parser.add_argument('-c', nargs='?', default='* * * * *')
+    parser.add_argument('-s', nargs='?', default='')
     name_space = parser.parse_args(sys.argv[1:])
     # create dict
     parameters = {"config_name": name_space.g, "cron_cmd": name_space.c, "config_slack": name_space.s}
